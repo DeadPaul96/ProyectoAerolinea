35456f7ee1aeb47ad645f3dae40bd58d84b37901
DeadPaul96 <jean.zgarcia1996@gmail.com>
2024-11-06 14:58:37 -0600

    JP

--- ProyectoFinalDs/src/main/java/pantallas/TiketsJP.java
+++ ProyectoFinalDs/src/main/java/pantallas/TiketsJP.java
@@ -6,32 +6,41 @@ package pantallas;
 
 import com.mycompany.proyectofinalds.Destino;
 import com.mycompany.proyectofinalds.DestinoControlador;
-import com.mycompany.proyectofinalds.DestinoController;
+import java.util.ArrayList;
 import java.util.List;
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
 
 /**
  *
  * @author jeanz
  */
 public class TiketsJP extends javax.swing.JPanel {
-DestinoControlador tc = new DestinoControlador();
 
-    /**
-     * Creates new form DestinoJP
-     */
-    public DestinoJP() {
+    private final DestinoControlador tc = new DestinoControlador();
+    private DefaultListModel<String> modeloLista = new DefaultListModel<>();
+    private JList<String> lista = new JList<>(modeloLista);
+    
+    public TiketsJP() {
         initComponents();
-        List<Destino> lista = tc.obtenerDestinos();
-        String[] data = new String[lista.size()];
-        int cont = 0;
-        for (Destino d : lista) {
-            data[cont]=d.getPais();
-            cont ++;
-        }
-        jlDestinos.setListData(data);
- 
 
+        List<Destino> destinos = tc.obtenerDestinos();
+        for (Destino destino : destinos) {
+            modeloLista.addElement(destino.getPais());
+        }
+        add(lista);
     }
+    public class DestinoControlador {
+        public List<Destino> obtenerDestinos() {
+            List<Destino> destinos = new ArrayList<>();
+            destinos.add(new Destino("Estados Unidos"));
+            destinos.add(new Destino("México"));
+            destinos.add(new Destino("Canadá"));
+            return destinos;
+        }
+    }
+    
+    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -42,14 +51,12 @@ DestinoControlador tc = new DestinoControlador();
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jScrollPane2 = new javax.swing.JScrollPane();
-        jlDestinos = new javax.swing.JList<>();
         jButton1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jfFechaSal = new javax.swing.JFormattedTextField();
-
-        jScrollPane2.setViewportView(jlDestinos);
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList<>();
 
         jButton1.setText("jButton1");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -65,6 +72,13 @@ DestinoControlador tc = new DestinoControlador();
         jfFechaSal.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.DateFormatter(new java.text.SimpleDateFormat("yyyy-MM-dd"))));
         jfFechaSal.setToolTipText("");
 
+        jList1.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane1.setViewportView(jList1);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -74,17 +88,18 @@ DestinoControlador tc = new DestinoControlador();
                 .addComponent(jButton1)
                 .addGap(22, 22, 22))
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(60, 60, 60)
-                        .addComponent(jLabel1))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(31, 31, 31)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jfFechaSal, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel2)
-                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addGap(78, 174, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(60, 60, 60)
+                            .addComponent(jLabel1))
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(31, 31, 31)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(jfFechaSal, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jLabel2)))))
+                .addGap(142, 238, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -95,13 +110,10 @@ DestinoControlador tc = new DestinoControlador();
                 .addComponent(jfFechaSal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 63, Short.MAX_VALUE)
                 .addComponent(jLabel2)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(181, 181, 181)
-                        .addComponent(jButton1))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(18, 18, 18)
-                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 173, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(17, 17, 17)
+                .addComponent(jButton1)
                 .addGap(20, 20, 20))
         );
     }// </editor-fold>//GEN-END:initComponents
@@ -115,8 +127,8 @@ DestinoControlador tc = new DestinoControlador();
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JList<String> jList1;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JFormattedTextField jfFechaSal;
-    private javax.swing.JList<String> jlDestinos;
     // End of variables declaration//GEN-END:variables
 }
